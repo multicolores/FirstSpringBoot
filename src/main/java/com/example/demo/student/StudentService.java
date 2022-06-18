117f142abd8c9b4b0aa490ef4fc4b7c617b31e95
Florian <florian.tellier02@gmail.com>
2022-06-18 17:46:34 +0200

    post fonctionne

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,13 +3,37 @@ package com.example.demo.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "Paul", "paul@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21));
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+        // System.out.println(student);
+    }
+
+    // public List<Student> getStudents() {
+    // return List.of(
+    // new Student(1L, "Paul", "paul@gmail.com", LocalDate.of(2000, Month.JANUARY,
+    // 5), 21));
+    // }
 }
